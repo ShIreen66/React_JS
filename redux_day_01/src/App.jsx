b1527b96cbca9b38d219faafdceb7575df9eb422
Shireen Khan <066shiree@gmail.com>
2025-06-10 12:45:27 +0530

    redux 001

---
+++ redux_day_01/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+
+const App = () => {
+  const data = useSelector((state) => state)
+  console.log(data)
+  return (
+    <div>App</div>
+  )
+}
+
+export default App
